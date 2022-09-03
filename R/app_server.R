50b5f15ee2234d4a32a358a87ed00e79ff1b8980
unknown <mviolati@outlook.com>
2022-09-03 17:03:50 +0200

    shiny app

---
+++ R/app_server.R
@@ -0,0 +1,14 @@
+#' The application server-side
+#'
+#' @param input,output,session Internal parameters for {shiny}.
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @noRd
+app_server <- function(input, output, session) {
+  # Your application server logic
+}
+
+
+# !! check() package first
+# !! Create a new git branch for the App
+# !! make the App based on the package already done
--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(ergodicityeconomics)
+library(eeconomics)
 
-test_check("ergodicityeconomics")
+test_check("eeconomics")
